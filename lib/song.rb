fe8750369d55a3555894677508c20265aa7ffd5f
Austin Kabiru <austin.kabiru@andela.com>
2016-03-21 12:37:58 +0300

    feat(search): find or create song
     - find song by name
     - return found song else create it

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,14 @@ class Song
 		genre_.add_song self
 	end
 
+	def self.find_by_name name
+		@@all.select { |song| song.name == name }.first
+	end
+
+	def self.find_or_create_by_name name
+		result = find_by_name name
+		result = create name unless result
+		result
+	end
+
 end
